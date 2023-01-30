a496b4b15a73d92cf317c80bc354ec73a2b19711
Aparna <sanagrawal10401@gmail.com>
2023-01-31 00:41:01 +0530

    debug

--- inference_seperate3.py
+++ inference_seperate3.py
@@ -115,7 +115,7 @@ def generate_video(stft, args, root, f, name):
     # Save the wav file
 
     audio_output = os.path.join(
-        result_dir, result_dir, f.split(".")[0]+"_result.wav")
+        result_dir, f.split(".")[0]+"_result.wav")
     librosa.output.write_wav(audio_output, wav, sampling_rate)
 
     print("Saved the denoised audio file:", audio_output)
